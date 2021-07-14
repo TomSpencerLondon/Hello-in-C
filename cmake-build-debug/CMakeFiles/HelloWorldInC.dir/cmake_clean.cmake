5b04d0d6cefb453a97c1c08ac572cb69c3c38188
Tom <thomas.spencer@codurance.com>
2021-07-14 23:23:12 +0100

    add code for C hello world

---
+++ cmake-build-debug/CMakeFiles/HelloWorldInC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldInC.dir/main.c.o"
+  "HelloWorldInC"
+  "HelloWorldInC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloWorldInC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
